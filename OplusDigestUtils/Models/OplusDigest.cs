bd09830ef68b949fbc4ea593a34825ff3bc8e320
XYCS <213067999+JacksonOnly@users.noreply.github.com>
2026-08-09 20:27:22 +0800

    ✨ feat(OplusDigestUtils): 添加整体和Header Model

---
+++ OplusDigestUtils/Models/OplusDigest.cs
@@ -0,0 +1,7 @@
+﻿namespace OplusDigestUtils.Models;
+
+public class OplusDigest
+{
+    public OplusDigestHeader Header { get; internal set; }
+    public IReadOnlyList<OplusDigestPartition> Partitions { get; internal set; } = [];
+}
---
+++ OplusDigestUtils/Models/OplusDigestHeader.cs
@@ -0,0 +1,12 @@
+﻿namespace OplusDigestUtils.Models;
+
+public class OplusDigestHeader
+{
+    public uint Magic { get; internal set; }
+    public uint Version { get; internal set; }
+    public uint HeaderSize { get; internal set; }
+    public uint PartitionNums { get; internal set; }
+    public uint PartitionTotalLength { get; internal set; }
+    public uint PartitionLength { get; internal set; }
+    public ulong Reserved { get; internal set; }
+}
